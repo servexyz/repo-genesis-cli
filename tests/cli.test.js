d3f7ec014e5ccf8c88c6830aa3a76f9fd00cb410
alechp <me@alechp.com>
2018-02-13 17:01:38 -0800

    Cosmetic

--- tests/cli.test.js
+++ tests/cli.test.js
@@ -3,11 +3,9 @@ import test from "ava";
 const log = console.log;
 const path = require("path");
 
-const cli = path.join(__dirname, "../cli.js");
-
-log(`Cli: ${cli}`);
 test("CLI has execution permission", t => {
+  const cli = path.join(__dirname, "../cli.js");
   const chmod = require("chmod");
-  let modifiedFile = chmod(cli, 777);
-  t.is(modifiedFile, undefined);
+  let modifiedCLI = chmod(cli, 777);
+  t.is(modifiedCLI, undefined);
 });
