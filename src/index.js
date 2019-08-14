467b65462d91e4413206abb239ad5c21a8019ddb
alechp <me@alechp.com>
2019-08-13 17:26:54 -0700

    added deps, started CLI & cleaned cruft

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log(`Hello from node-starter!`);
+#!usr/bin/env node
+
+const log = console.log;
+import path from "path";
+
+import * as rg from "repo-genesis-lib";
+import meow from "meow";
+
+(async () => {
+  await rg.parse();
+})();
