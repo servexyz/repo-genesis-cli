026998b4acaa5f920281a28cc46d866e91385c86
alechp <me@alechp.com>
2019-08-15 10:40:41 -0700

    cli working with hashbang

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-#!usr/bin/env node
-
 const log = console.log;
 import path from "path";
 import ora from "ora";
+import chalk from "chalk";
 import * as tkr from "tacker";
 
 import * as rg from "repo-genesis-lib";
 import meow from "meow";
 
-(async () => {
+// (async () => {
+//   const spinner = ora("Cloning repositories");
+//   spinner.color = "blue";
+//   let config = await rg.parse();
+//   spinner.text = "Cloning repositories from git";
+//   spinner.start();
+//   await rg.genAll(config);
+//   spinner.stop();
+// })();
+
+let helpMenu = `${tkr.printLine("blue")}
+Usage
+  $ repogen <command>
+
+Commands
+  * repogen ${chalk.cyan("help")}
+`;
+const cli = meow(helpMenu, {});
+
+async function init() {
   const spinner = ora("Cloning repositories");
-  let config = await rg.parse();
-  spinner.start();
-  await rg.genAll(config);
-  spinner.stop();
-})();
+  spinner.color = "blue";
+  try {
+    let config = await rg.parse();
+    spinner.text = "Cloning repositories from git";
+    spinner.start();
+    await rg.genAll(config);
+    spinner.succeed();
+  } catch (err) {
+    return tkr.printError(true, { fn: "init", err });
+  }
+}
+
+init();
