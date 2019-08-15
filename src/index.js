f6b9bc7be6735fea0fb227557d6c580c8bbff9f9
alechp <me@alechp.com>
2019-08-15 09:39:23 -0700

    spinner working

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@
 
 const log = console.log;
 import path from "path";
+import ora from "ora";
+import * as tkr from "tacker";
 
 import * as rg from "repo-genesis-lib";
 import meow from "meow";
 
 (async () => {
+  const spinner = ora("Cloning repositories");
   let config = await rg.parse();
+  spinner.start();
   await rg.genAll(config);
+  spinner.stop();
 })();
